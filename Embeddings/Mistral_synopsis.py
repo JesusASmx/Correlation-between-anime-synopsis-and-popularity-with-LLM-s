ed2c9b10c091195c42ab22b14661eb81b371d1ae
Jesús Armenta-Segura <138358307+JesusASmx@users.noreply.github.com>
2024-03-13 16:34:24 -0600

    Add files via upload

--- Embeddings/Mistral_synopsis.py
+++ Embeddings/Mistral_synopsis.py
@@ -30,31 +30,29 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def get_embs(seed, temp, model, df, text, split, bsize, start, proxy):
     samples = df[text].tolist()[start*bsize:min((start+1)*bsize, len(df[text].tolist()))]
-    #print(f"EXAMPLE OF SAMPLE: {samples[0][-15:]}")
     tensors = []
     for x in tqdm(samples, desc= model+" embeddings"):
-        array = get_ollama_emb(prompt=x, seed=seed, temp=temp, model=model, proxy=proxy)
+        array = get_ollama_emb(prompt=x, seed=seed, temp=temp, model=model)
         tensors.append(array)
     tensors = torch.stack(tensors, dim=0).to(device)
     print(tensors.shape)
-    torch.save(tensors, './/LLAMA2//'+model+f'_syn_score_{split}_batch{str(start)}.pt')
+    torch.save(tensors, './/MISTRAL//'+model+f'_syn_score_{split}_batch{str(start)}.pt')
     print("Tensors successfully saved!")
-
 
 import math
 bsize = 32
-bstart_train = 0
+bstart_train = 20
 print(len(train_df))
 
 your_proxy = f'http://{HERE YOU PUT YOUR LOCAL ADDRESS FOR OLLAMA}/api/embeddings'
 
 print(f"Starting batch: {bstart_train}\nTotal batches: {math.ceil(len(train_df)/bsize)}")
 for x in tqdm(range(bstart_train, math.ceil(len(train_df)/bsize)), desc="Batch"):
-    get_embs(seed=42, temp=0.2, model="llama2", df=train_df, text="Synopsis", split="train", bsize=bsize, start=x, proxy=your_proxy)
+    get_embs(seed=42, temp=0.2, model="mistral", df=train_df, text="Synopsis", split="train", bsize=bsize, start=x, proxy=your_proxy)
 
 bstart_test = 0
 for x in tqdm(range(bstart_test, math.ceil(len(test_df)/bsize)), desc="Batch"):
-    get_embs(seed=42, temp=0.2, model="llama2", df=test_df, text="Synopsis", split="test", bsize=bsize, start=x, proxy=your_proxy)
+    get_embs(seed=42, temp=0.2, model="mistral", df=test_df, text="Synopsis", split="test", bsize=bsize, start=x, proxy=your_proxy)
 
 #a = get_ollama_emb(prompt = frieren, seed = 42, temp = 0.2, "mistral")
 #print(a)
